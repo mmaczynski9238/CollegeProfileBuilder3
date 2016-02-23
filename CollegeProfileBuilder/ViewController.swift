ad628df046b7ea38bf36dff687c00d5e0baf82db
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-02-23 14:47:57 -0600

    added basic functionality

--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var colleges = ["Chicago", "San Fransisco", "Dallas", "Paris", "Rome", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       }
-
-
-}
+        
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+    }
+    
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return colleges.count
+    }
+    
+    
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        var myCell = myTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
+        myCell.textLabel?.text = colleges[indexPath.row]
+        return myCell
+        
+    }}
 
